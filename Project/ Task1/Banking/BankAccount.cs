42f0a45a5af1ce6dac9ecdceb7eed6a9434fbc96
Mphiliseni <Mphilisenialexmhlongo@gmail.com>
2025-06-23 19:21:45 +0200

    Add initial Banking project structure and implementation

---
+++ Project/ Task1/Banking/BankAccount.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Banking
+{
+    public class BankAccount
+    {
+        //TODO Task 1.1
+        public string AccountNumber { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+        
+        //END TODO Task 1.1
+
+    }
+}
---
+++ Project/ Task1/Banking/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Banking;
+
+namespace Banking
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //TODO Task 1.2
+            BankAccount account = new BankAccount
+            {
+                AccountNumber = "123456789",
+                Balance = 1000.23m
+            };
+            Console.WriteLine($"Account Number: {account.AccountNumber} Balance: {account.Balance:C}");
+            //END TODO Task 1.2
+        }
+    }
+}
