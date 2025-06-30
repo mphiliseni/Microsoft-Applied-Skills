665ca13388aa8cf21b5d7d8bfd4192c72f62f680
Mphiliseni <Mphilisenialexmhlongo@gmail.com>
2025-06-30 12:18:28 +0200

    Update assembly informational version and modify Calculator Program for new addition implementation

--- Project/Task3/Calculator/Program.cs
+++ Project/Task3/Calculator/Program.cs
@@ -4,9 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        double num1 = 5;
-        double num2 = 3;
-        double result = Calculator.Addition(num1, num2);
-        Console.WriteLine($"The sum is: {result}");
+        double result = Addition(1.6, 3.6);
+        Console.WriteLine(result);
     }
 }
--- Project/Task3/Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ Project/Task3/Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efdb5f8e10aaf0d3fc49c4a1c9de9fa85d17adea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4f29b666315684cc32911e340037f28864583b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
